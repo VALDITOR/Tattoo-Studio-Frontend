147be8f9b0f94fb68163cc40f444326f7cf30196
Valditor <jandrovaldi@gmail.com>
2023-11-10 13:51:33 +0100

    Create: registration section

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,120 @@
+import React, { useState, useEffect } from "react";
+import "./Register.css";
 
+import { CustomInput } from "../../common/CustomInput/CustomInput";
+import { validator } from "../../services/useful";
+import { registerUser } from "../../services/apiCalls";
+import { useNavigate } from 'react-router-dom';
 
-import React from 'react';
-import './Register.css'
 export const Register = () => {
-     return (
-         <div className="registerDesign"></div>
-     )
-}
+
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    password: ''
+  })
+
+  const [userError, setUserError] = useState({
+    nameError: '',
+    surnameError: '',
+    emailError: '',
+    passwordError: ''
+  })
+
+
+  const functionHandler = (e) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const errorCheck = (e) => {
+
+    let error = "";
+
+    error = validator(e.target.name, e.target.value);
+
+    setUserError((prevState) => ({
+        ...prevState,
+        [e.target.name + 'Error']: error,
+    }));
+  }
+
+  const Submit = () => {
+
+    for(let test1 in user){
+      if(user[test1] === ""){
+        return;
+      }
+
+    }
+
+    for(let test in userError){
+      if(userError[test] !== ""){
+        return;
+      }
+    }
+
+    registerUser(user)
+      .then(
+        resultado => {
+          //si todo ha ido bien, redirigiremos a login...
+
+          setTimeout(()=>{
+            navigate("/login");
+          },500)
+        }
+      )
+      .catch(error=> console.log(error));
+  }
+
+  return (
+    <div className="registerDesign">
+      <CustomInput
+        design={`inputDesign ${userError.emailError !== "" ? 'inputDesignError' : ''}`}
+        type={"email"}
+        name={"email"}
+        placeholder={""}
+        // value={}
+        functionProp={functionHandler}
+        functionBlur={errorCheck}
+      />
+      <div className='errorMsg'>{userError.emailError}</div>
+      <CustomInput
+        design={`inputDesign ${userError.passwordError !== "" ? 'inputDesignError' : ''}`}
+        type={"password"}
+        name={"password"}
+        placeholder={""}
+        // value={}
+        functionProp={functionHandler}
+        functionBlur={errorCheck}
+      />
+      <div className='errorMsg'>{userError.passwordError}</div>
+      <CustomInput
+        design={`inputDesign ${userError.nameError !== "" ? 'inputDesignError' : ''}`}
+        type={"text"}
+        name={"name"}
+        placeholder={""}
+        // value={}
+        functionProp={functionHandler}
+        functionBlur={errorCheck}
+      />
+      <div className='errorMsg'>{userError.nameError}</div>
+      <CustomInput
+        design={`inputDesign ${userError.surnameError !== "" ? 'inputDesignError' : ''}`}
+        type={"text"}
+        name={"phone"}
+        placeholder={""}
+        // value={}
+        functionProp={functionHandler}
+        functionBlur={errorCheck}
+      />
+      <div className='errorMsg'>{userError.surnameError}</div>
+      <div className='buttonSubmit' onClick={Submit}>Submit</div>
+    </div>
+  );
+};
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -11,3 +11,9 @@ export const logUser = async (body) => {
     // return `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMsImVtYWlsIjoiZGF2aWRAZGF2aWQuY29tIiwiaWF0IjoxNjk5NTMwMDY1LCJleHAiOjE2OTk1NDA4NjV9.UNMBvL7RwP6JylDq9Ut4r5ach9HGVKqBm-5yv0-w_B4`;
 
 }
+
+export const registerUser = async (body) => {
+    //SIMULACRO DE CONEXION REAL A API
+    // return await axios.post(`elendpointdemipreciosobackend`, body);
+    return "todo ha ido bien"
+}
