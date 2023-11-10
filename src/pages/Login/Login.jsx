dff7847e49df17197c97313dc511c5e125255d2d
Valditor <jandrovaldi@gmail.com>
2023-11-10 09:31:21 +0100

    Iniciated the project

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import "./Login.css";
+import { CustomInput } from "../../common/CustomInput/CustomInput";
+import { logUser } from "../../services/apiCalls";
+import { useNavigate } from 'react-router-dom';
+
+export const Login = () => {
+
+  const navigate = useNavigate();
+
+  const [credenciales, setCredenciales] = useState({
+    email: "",
+    password: "",
+  });
+
+  const functionHandler = (e) => {
+    setCredenciales((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+//   useEffect(()=>{
+//     console.log(credenciales);
+//   },[credenciales]);
+
+  const logMe = () => {
+
+    logUser(credenciales)
+        .then(
+            resultado => {
+                console.log(resultado)
+                //Aqui guardaría el token........
+
+                //Una vez guardado el token....nos vamos a home....
+                setTimeout(()=>{
+                    navigate("/");
+                },500);
+            }
+        )
+        .catch(error => console.log(error));
+
+  }
+
+  return (
+    <div className="loginDesign">
+      <CustomInput
+        design={"inputDesign"}
+        type={"email"}
+        name={"email"}
+        placeholder={""}
+        // value={}
+        functionProp={functionHandler}
+        // onBlur={}
+      />
+      <CustomInput
+        design={"inputDesign"}
+        type={"password"}
+        name={"password"}
+        placeholder={""}
+        // value={}
+        functionProp={functionHandler}
+        // onBlur={}
+      />
+      <div className='buttonSubmit' onClick={logMe}>Log Me!</div>
+    </div>
+  );
+};
