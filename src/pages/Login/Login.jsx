3b6f38e5beb9a63ece7be363d3ffdcab93edcd6e
Valditor <jandrovaldi@gmail.com>
2023-11-13 21:27:55 +0100

    Funtion: recover the token when logging in using redux

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,15 +4,21 @@ import { CustomInput } from "../../common/CustomInput/CustomInput";
 import { validator } from "../../services/useful";
 import { logUser } from "../../services/apiCalls";
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from "react-redux";  //useDispatch es necesario para emitir acciones
+import { login } from "../userSlice";
 
 export const Login = () => {
 
   const navigate = useNavigate();
 
+  const dispatch = useDispatch();
+
   const [credenciales, setCredenciales] = useState({
     email: "",
     password: "",
   });
+
+  const [msgError, setMsgError] = useState('');
 
   const functionHandler = (e) => {
     setCredenciales((prevState) => ({
@@ -20,6 +26,10 @@ export const Login = () => {
       [e.target.name]: e.target.value
     }));
   };
+
+  const errorCheck = () => {
+    console.log("ha ha ha ha");
+  }
 
 //   useEffect(()=>{
 //     console.log(credenciales);
@@ -31,7 +41,8 @@ export const Login = () => {
         .then(
             resultado => {
                 console.log(resultado)
-                //Aqui guardaría el token........
+                //Aqui guardaría el token........en RDXXX
+                dispatch(login({ credentials: resultado.data }))
 
                 //Una vez guardado el token....nos vamos a home....
                 setTimeout(()=>{
@@ -39,7 +50,10 @@ export const Login = () => {
                 },500);
             }
         )
-        .catch(error => console.log(error));
+        .catch(error => {
+          console.log(error)
+          setMsgError(error.message);
+        });
 
   }
 
