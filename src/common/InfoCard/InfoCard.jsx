7958490c1edec60769adfb9a74f846cbdc5808cd
Valditor <jandrovaldi@gmail.com>
2023-11-14 13:06:13 +0100

    Add: react icons library

--- src/common/InfoCard/InfoCard.jsx
+++ src/common/InfoCard/InfoCard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import './InfoCard.css'
 import { Link } from 'react-router-dom'
 
-    <InfoCard logo={e.logo} title={e.title} text={e.text} url={e.url}/>
+    <InfoCard logo="logo" title="title" text="text" url="url"/>
 
 export default function InfoCard({logo, title, text, url}) {
     return (
         <div className='InfoCardDesign'>
             <div className='InfoCardLogo'>
-            <img src={logo}/>
-        </div>
+                {logo}
+            </div>
         <div className='InfoCardTitle'>
             <p>
                 {title}
--- src/pages/Information/information.jsx
+++ src/pages/Information/information.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './information.css'
 import InfoCard from '../../common/InfoCard/InfoCard';
+import Logo from '../../img/logo.jpg'
+import { logUser } from '../../services/apiCalls';
+import { MdHealthAndSafety } from "react-icons/md";
+
 
 export const Information = () => {
      return (
          <div className="informationDesign">
-            <InfoCard />
+            <div className='informationDivider'>
+            <InfoCard logo={<MdHealthAndSafety  className='color'/>} title="loem" text="texto" url="angel" />
+            </div>
+            <div className='informationDivider'>
+            <InfoCard logo={Logo} title="gaston" text="texto" url="angel" />
+            </div>
+            <div className='informationDivider'>
+            <InfoCard logo={Logo} title="gaston" text="texto" url="angel" />
+            </div>
          </div>
      )
 }
